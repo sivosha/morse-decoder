10cf8a208123a8f03282805d9bd9a071af91d7ff
sivosha <sivosha1@gmail.com>
2023-01-24 14:35:02 -0700

    init: solution

--- src/index.js
+++ src/index.js
@@ -36,8 +36,37 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
+function deci(twoNum){
+    if (twoNum == '10'){
+        return '.';
+    }
+    else if(twoNum == '11'){
+        return '-';
+    }
+    else{
+        return '';
+    }    
+}
 
 function decode(expr) {
+    var result=[];
+    const letters = expr.match(/.{1,10}/g) ?? [];
+    for (var i =0; i<letters.length; i++){
+        if ( letters[i] == '**********'){
+            result.push(" ");
+        }
+        else {
+            var symbol = [];
+            const divLeter = letters[i].match(/.{1,2}/g) ?? [];
+            for(var j =0; j<divLeter.length; j++){
+                symbol.push(deci(divLeter[j]));
+            }
+            console.log(symbol.join(''));
+            result.push(MORSE_TABLE[symbol.join('')]);
+        }
+    }
+    return result.join('');
+    
     // write your solution here
 }
 
